157a4340a6afc1448907d89cf5daeeac41cdd34e
hossainshadat <hossainshadat501@gmail.com>
2022-10-12 18:44:12 +0600

    Blog page done

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -43,61 +43,47 @@ export const Blog = () => {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 bg-zinc-100 ">
       <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
-        <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-          <div>
-            <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-              Brand new
-            </p>
-          </div>
-          <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-            <span className="relative inline-block">
-              <svg
-                viewBox="0 0 52 24"
-                fill="currentColor"
-                className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-              >
-                <defs>
-                  <pattern
-                    id="232db96b-4aa2-422f-9086-5a77996d1df1"
-                    x="0"
-                    y="0"
-                    width=".135"
-                    height=".30"
-                  >
-                    <circle cx="1" cy="1" r=".7" />
-                  </pattern>
-                </defs>
-                <rect
-                  fill="url(#232db96b-4aa2-422f-9086-5a77996d1df1)"
-                  width="52"
-                  height="24"
-                />
-              </svg>
-              <span className="relative">The</span>
-            </span>{" "}
-            quick, brown fox jumps over a lazy dog
-          </h2>
-          <p className="text-base text-gray-700 md:text-lg ">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
-          </p>
-        </div>
         <div className="space-y-4">
-          <Item title="The quick, brown fox jumps over a lazy dog?">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
+          <Item title="What is the purpose of React Router?">
+            Routing is a process in which a user is directed to different pages
+            based on their action or request. ReactJS Router is mainly used for
+            developing Single Page Web Applications. React Router is used to
+            define multiple routes in the application. When a user types a
+            specific URL into the browser, and if this URL path matches any
+            'route' inside the router file, the user will be redirected to that
+            particular route. React Router is a standard library system built on
+            top of the React and used to create routing in the React application
+            using React Router Package. It provides the synchronous URL on the
+            browser with data that will be displayed on the web page. It
+            maintains the standard structure and behavior of the application and
+            mainly used for developing single page web applications.
           </Item>
-          <Item title="The first mate and his Skipper too will do?">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
+          <Item title="How does context API work?">
+            One of the concepts of React is to break your application into
+            components, for reusability purposes. So in a simple React
+            application, we have a few different components. As our application
+            grows, these components can become huge and unmaintainable, so we
+            break them into smaller components. That’s one of the best concepts
+            about React—you can create a bunch of components and have a fully
+            maintainable and concise application, without having to create a
+            super huge component to deal with your whole application. After
+            breaking components into smaller components for maintainability
+            purposes, these small components might now need some data to work
+            properly. If these small components need data to work with, you will
+            have to pass data through props from the parent component to the
+            child component. This is where we can slow down our application and
+            cause development issues.
           </Item>
-          <Item title="Is the Space Pope reptilian!?">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
-          </Item>
-          <Item title="How much money you got on you?">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
+          <Item title="useRef() hook React?">
+            useRef() hook creates references. Calling const reference =
+            useRef(initialValue) with the initial value returns a special object
+            named reference. The reference object has a property current: you
+            can use this property to read the reference value reference.current,
+            or update reference.current = newValue. Between the component
+            re-renderings, the value of the reference is persistent. Updating a
+            reference, contrary to updating state, doesn't trigger component
+            re-rendering. References can also access DOM elements. Assign the
+            reference to ref attribute of the element you'd like to access:{" "}
           </Item>
         </div>
       </div>
